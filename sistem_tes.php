14749e9cbb5e90a2feee55adadaf1c6833bf6983
joker-saputra <66672714+joker-saputra@users.noreply.github.com>
2020-07-07 21:40:19 +0700

    Update sistem_tes.php

--- sistem_tes.php
+++ sistem_tes.php
@@ -1,21 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Sistem_aplikasi extends CI_Model {
-	public function FunctionName($value='')
-	{
-		echo "string";
-	}
-	function version(){
+class Sistem extends CI_Model {
+	function versi(){
 		return '0.1';
-	}
-	function sistem_update(){
-		 $connected = @fsockopen("www.google.com", 80);
-		 if ($connected){
-			$link = 'https://raw.githubusercontent.com/joker-saputra/global/master/sistem_tes.php';
-			$update = 'application/models/Sistem_aplikasi.php';
-		  	$is_conn = copy($link, $update);
-		  	fclose($connected);
-		 }
 	}
 }
